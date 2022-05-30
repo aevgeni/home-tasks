4e4cfd1e975dae86021fa3e47a5a30fd8ba04b0d
Evgeniy Antonovich <gnoptswbn5kj@mail.ru>
2022-05-30 15:35:57 +0300

    style: rename class and file task2 to Task2

--- lesson-1/src/Task2.java
+++ lesson-1/src/Task2.java
@@ -1,4 +1,4 @@
-public class task2 {
+public class Task2 {
     public static void main(String[] args) {
         System.out.println("Java: version 11.");
         System.out.println("JVM: HotSpot.");
